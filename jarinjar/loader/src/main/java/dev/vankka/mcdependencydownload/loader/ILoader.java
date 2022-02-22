e4a61fb6b6267eeade14ca13aaceb05a379b9702
Vankka <vankka.main@gmail.com>
2022-02-22 18:28:54 +0200

    Make ILoader more extensible, make more stuff optional

--- jarinjar/loader/src/main/java/dev/vankka/mcdependencydownload/loader/ILoader.java
+++ jarinjar/loader/src/main/java/dev/vankka/mcdependencydownload/loader/ILoader.java
@@ -4,9 +4,10 @@ import dev.vankka.mcdependencydownload.classloader.JarInJarClassLoader;
 import dev.vankka.mcdependencydownload.loader.exception.LoadingException;
 import org.jetbrains.annotations.ApiStatus;
 import org.jetbrains.annotations.MustBeInvokedByOverriders;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.UnknownNullability;
 
 import java.io.IOException;
-import java.lang.reflect.InvocationTargetException;
 import java.net.URL;
 
 /**
@@ -16,28 +17,27 @@ import java.net.URL;
 public interface ILoader {
 
     /**
-     * Initializes the {@link JarInJarClassLoader} and loads in the {@link #getBootstrapClassName()}, and then calls {@link #initiateBootstrap(Class, JarInJarClassLoader)} with that class the the class loader made previously.
-     * @throws LoadingException if initialization fails
+     * Initializes the {@link JarInJarClassLoader} and loads in the {@link #getBootstrapClassName()}, and then calls {@link #initiateBootstrap(Class, JarInJarClassLoader)} with that class the class loader made previously.
+     * Calls {@link #handleLoadingException(LoadingException)} if initialization fails, by default this throws the exception
+     *
+     * @return the {@link JarInJarClassLoader} used to load the bootstrap, this should be closed when not needed anymore
+     *
+     * @see #createClassLoader()
+     * @see #initiateBootstrap(Class, JarInJarClassLoader)
      */
     @MustBeInvokedByOverriders
     @ApiStatus.NonExtendable
-    default void initialize() {
+    @UnknownNullability("Not null unless handleLoadingException is modified")
+    default JarInJarClassLoader initialize() {
         try {
-            JarInJarClassLoader classLoader = new JarInJarClassLoader(getName(), getJarInJarResource(), getParentClassLoader());
-
+            JarInJarClassLoader classLoader = createClassLoader();
             Class<?> clazz = classLoader.loadClass(getBootstrapClassName());
             initiateBootstrap(clazz, classLoader);
 
-            Runtime.getRuntime().addShutdownHook(new Thread(() -> {
-                try {
-                    classLoader.close();
-                } catch (IOException e) {
-                    System.err.println("Failed to shutdown JarInJarClassLoader");
-                    e.printStackTrace();
-                }
-            }, getName() + " JarInJarClassLoader ShutdownHook"));
+            return classLoader;
         } catch (Throwable t) {
             handleLoadingException(new LoadingException("Unable to load JarInJar", t));
+            return null;
         }
     }
 
@@ -45,44 +45,55 @@ public interface ILoader {
      * {@link LoadingException} handler for this {@link ILoader}, by default the exception is simply thrown into the constructor through {@link #initialize()}.
      * @param exception the loading exception
      */
-    default void handleLoadingException(LoadingException exception) {
+    default void handleLoadingException(@NotNull LoadingException exception) {
         throw exception;
     }
 
     /**
-     * The fully qualified class name for the bootstrap class, this class usually extends {@code AbstractBootstrap}.
-     * @return the bootstrap class name
+     * Creates the class loader for this {@link ILoader}.
+     * @return the new classloader
      */
-    String getBootstrapClassName();
+    @NotNull
+    default JarInJarClassLoader createClassLoader() throws IOException {
+        return new JarInJarClassLoader(getName(), getJarInJarResource(), getParentClassLoader());
+    }
+
+    /**
+     * The parent {@link ClassLoader} that loaded in this loader, this is used in {@link #createClassLoader()} by default.
+     * @return the parent {@link ClassLoader}
+     */
+    @NotNull
+    default ClassLoader getParentClassLoader() {
+        return getClass().getClassLoader();
+    }
 
     /**
      * Get the constructor from the class and create a new instance, the provided {@link JarInJarClassLoader} should be used in the constructor.
      *
      * @param bootstrapClass the bootstrap class resolved from {@link #getBootstrapClassName()}
      * @param classLoader the {@link JarInJarClassLoader} that was used to load the bootstrap class and should be provided in the constructor
-     * @throws NoSuchMethodException a convenience throw, will be thrown as {@link LoadingException} in {@link #initialize()}
-     * @throws InvocationTargetException a convenience throw, will be thrown as {@link LoadingException} in {@link #initialize()}
-     * @throws InstantiationException a convenience throw, will be thrown as {@link LoadingException} in {@link #initialize()}
-     * @throws IllegalAccessException a convenience throw, will be thrown as {@link LoadingException} in {@link #initialize()}
+     * @throws ReflectiveOperationException a convenience throw, will be passed as a {@link LoadingException} to {@link #handleLoadingException(LoadingException)}
      */
-    void initiateBootstrap(Class<?> bootstrapClass, JarInJarClassLoader classLoader)
-            throws NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException;
+    void initiateBootstrap(@NotNull Class<?> bootstrapClass, @NotNull JarInJarClassLoader classLoader) throws ReflectiveOperationException;
 
     /**
-     * Returns the name of the plugin/mod/extension this is used for the jarinjar file name which is stored in the system's temporary file directory and shutdown hook thread name.
-     * @return the name of the plugin/mod/extension
+     * The fully qualified class name for the bootstrap class, this class usually extends {@code AbstractBootstrap}.
+     * @return the bootstrap class name
      */
+    @NotNull
+    String getBootstrapClassName();
+
+    /**
+     * Returns the name of this loader this is used for the jarinjar file name which is stored in the system's temporary file directory.
+     * @return the name of this loader
+     */
+    @NotNull
     String getName();
-
-    /**
-     * The parent {@link ClassLoader} that loaded in this loader, this is used for the {@link JarInJarClassLoader}.
-     * @return the parent dev.vankka.mcruntimedependencydownload.bootstrap.classloader
-     */
-    ClassLoader getParentClassLoader();
 
     /**
      * The {@link URL} to the JarInJar resource that contains the bootstrap.
      * @return the url to the JarInJar resource
      */
+    @NotNull
     URL getJarInJarResource();
 }
